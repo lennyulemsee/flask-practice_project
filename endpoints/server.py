615bca5fb23e2d200f316226a954fd7fcb84e57d
Leonard Muchuki <lennymkuu@gmail.com>
2024-02-26 18:59:23 +0300

    complied to pylint

--- endpoints/server.py
+++ endpoints/server.py
@@ -1,8 +1,8 @@
+""" A simple flask app that handles CRUD """
 from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
-""" A simple flask app that handles CRUD """
 data = [
     {
         "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
@@ -67,49 +67,46 @@ def get_data():
     try:
         if data and len(data) > 0:
             return {"message": f"Data of length {len(data)} found"}
-        else:
-            return {"message": "Data is empty"}, 500
-    
+        return {"message": "Data is empty"}, 500
     except NameError:
         return {"message": "Data not found"}, 404
 
 @app.route("/name_search")
 def name_search():
-    """ searches for the name given in the url parameters """ 
+    """ searches for the name given in the url parameters """
     fr_name = request.args.get("q")
     if not fr_name:
         return ({"message": "Invalid input parameter"}, 422)
-    else:
-        for person in data:
-            #if person["first_name"] == fr_name: -mine
-            if fr_name.lower() in person["first_name"].lower():
-                return person, 200
-        return ({"message": "Person not found"}, 404)
+    for person in data:
+        #if person["first_name"] == fr_name: -mine
+        if fr_name.lower() in person["first_name"].lower():
+            return person, 200
+    return ({"message": "Person not found"}, 404)
 
 @app.route("/count")
 def count():
     """ counts the number of people stored """
     try:
-        ct = len(data)
-        return {"Data count": ct}, 200
+        people_ct = len(data)
+        return {"Data count": people_ct}, 200
     except NameError:
         return {"message": "Data not defined"}, 500
 
-@app.route("/person/<uuid:id>")
-def find_by_uuid(id):
+@app.route("/person/<uuid:uniq_id>")
+def find_by_uuid(uniq_id):
     """ Searches for the person with the given UUID """
     for person in data:
-        if str(id) == person["id"]:
+        if str(uniq_id) == person["id"]:
             return person, 200
     return ({"message": "Person not found"}, 404)
 
-@app.route("/person/<uuid:id>", methods=["DELETE"])
-def delete_by_uuid(id):
+@app.route("/person/<uuid:uniq_id>", methods=["DELETE"])
+def delete_by_uuid(uniq_id):
     """ Deletes the person with the given uuid """
     for person in data:
-        if str(id) == person["id"]:
+        if str(uniq_id) == person["id"]:
             data.remove(person)
-            return {"message": f"Person of id {id} has been deleted successifully."}, 200
+            return {"message": f"Person of id {uniq_id} has been deleted successifully."}, 200
     return ({"message": "Person not found"}, 404)
 
 @app.route("/person", methods=["POST"])
@@ -125,8 +122,8 @@ def add_by_uuid():
 
     return {"message": new_person["id"]}, 200
 
-## Sample data to add
 """
+## Sample data to add
 curl -X POST -i -w '\n' \
   --url http://localhost:5000/person \
   --header 'Content-Type: application/json' \
@@ -151,6 +148,7 @@ def api_not_found(error):
     return {"message": "Api not found"}, 404
 @app.route("/no_content")
 def no_content():
+    """ no content """
     return "No content found", 204
 
 @app.route("/exp")
@@ -159,5 +157,3 @@ def index_explicit():
     resp = make_response({"message": "Hello world"})
     resp.status_code = 200
     return resp
-
-
